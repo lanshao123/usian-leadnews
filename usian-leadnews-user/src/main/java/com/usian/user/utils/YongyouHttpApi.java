da43d6e7951db1875703221251f73f18e6a4db62
wh <cqmm60@163.com>
2022-08-09 11:50:00 +0800

    2022-8-9-进行了实名逻辑处理

---
+++ usian-leadnews-user/src/main/java/com/usian/user/utils/YongyouHttpApi.java
@@ -0,0 +1,23 @@
+package com.usian.user.utils;
+
+/**
+ * @program: usian-leadnews
+ * @description: YongyouHttpApi
+ * @author: wangheng
+ * @create: 2022-08-09 09:30
+ **/
+public class YongyouHttpApi {
+    //orc
+    public final static String OCRURL="https://api.yonyoucloud.com/apis/dst/IdcardOCR/IdcardOCR";
+    public final static String OCRCODE="8c9dd2f101b344bab1ae3b27ecabe1a5";
+    //身份证二要素
+    public final static String MATCHIDENTITY ="https://api.yonyoucloud.com/apis/dst/matchIdentity/matchIdentity";
+    public final static String MATCHIDENTITYCODE="ec3249d5539d4e94a0d26f8a6e0b6db7";
+    //活体检测
+    public final static String BIOLOGICALEXAMINATION ="https://api.yonyoucloud.com/apis/dst/Biologicalexamination/Biologicalexamination";
+    public final static String BIOLOGICALEXAMINATIONCODE="392211c086c6402ca0a56a10cda1d683";
+    //人证核验
+    public final static String COMPARISONSOFPERSONSANDDOCUMENTS ="https://api.yonyoucloud.com/apis/dst/ComparisonsofPersonsandDocuments/ComparisonsofPersonsandDocuments";
+    public final static String COMPARISONSOFPERSONSANDDOCUMENTSCODE="b285b7b8d5874f4fb70b35ebddf0b15c";
+
+}
